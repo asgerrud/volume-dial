9cd8beeb6277ef3690373b89bbfdf4ec430f06ad
AsgereDreemurr <36601572+AsgereDreemurr@users.noreply.github.com>
2022-01-21 22:53:54 +0100

    Use Socket.IO for server-client communication
    - Use Socket.IO instead of PubNub
    - Remove jQuery dependency
    - Code cleanup

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,12 @@
-const degreeToPercent = (deg, min, max) => {
-  return Math.round(((deg - min) / (max - min)) * 100);
-};
+const degreeToPercent = (deg, min, max) => Math.round(((deg - min) / (max - min)) * 100);
+const percentToDegree = (percent, min, max) => (percent / 100) * (max - min) + min;
 
-const percentToDegree = (percent, min, max) => {
-  return (percent / 100) * (max - min) + min;
-};
+const START_VOLUME = 50;
+const INCREMENT = 5;
+const MIN_DEG = 180;
+const MAX_DEG = 360 + MIN_DEG;
+const START_DEG = percentToDegree(START_VOLUME, MIN_DEG, MAX_DEG);
+const NUM_TICKS = 100 / INCREMENT;
 
 const vibrate = (percent, tick = 25, stop = 50) => {
   window.navigator.vibrate(tick);
@@ -13,41 +15,67 @@ const vibrate = (percent, tick = 25, stop = 50) => {
   }
 };
 
-const changeWindowsAudio = percent => {
-  pubnub.publish(
-    {
-      channel: ["CHANNEL KEY GOES HERE"],
-      message: {
-        type: "SYSTEM_AUDIO",
-        volume: percent
-      }
-    },
-    status => {
-      // Pubnub does not guarantee delivery. However, we can assume this if the message was delivered without errors
-      if (status.error === false) {
-        vibrate(percent);
-        console.log("New volume: ", percent);
-      }
-    }
-  );
+const setDial = percent => {
+  dial.angle(percentToDegree(percent, MIN_DEG, MAX_DEG));
 };
 
-const handleVolumeChange = (rotation, minDeg, maxDeg) => {
-  let percent = degreeToPercent(rotation, minDeg, maxDeg);
-  let activeTicks = Math.round((percent / 100) * NUM_TICKS + 1);
+const receiveVolume = percent => {
+  handleVolumeChange(percent, false);
+};
+
+const handleVolumeChange = (percent, emitChange = true) => {
+  const activeTicks = Math.round((percent / 100) * NUM_TICKS + 1);
 
   if (activeTicks !== VolumeDial.activeTicks) {
     // Highlight ticks
-    $(".tick").removeClass("active");
-    $(".tick")
-      .slice(0, activeTicks)
-      .addClass("active");
-    // Update number of active ticks
+    const ticks = document.querySelectorAll('.tick');
+    ticks.forEach(el => el.classList.remove('active'));
+    [...ticks].slice(0, activeTicks).forEach(el => el.classList.add('active'));
     VolumeDial.activeTicks = activeTicks;
   }
-
   if (percent !== VolumeDial.volume && percent % INCREMENT == 0) {
     VolumeDial.volume = percent;
-    changeWindowsAudio(percent);
+    vibrate(percent);
+    if (emitChange) socket.emit('volume-change', percent);
   }
 };
+
+var VolumeDial = {
+  id: 'VolumeDial',
+  volume: START_VOLUME,
+  activeTicks: 0,
+};
+
+const config = {
+  debug: false,
+  touchMode: 'wheel',
+  knobSize: '30px',
+  wheelSize: '200px',
+  zIndex: 9999,
+  degreeStartAt: 90,
+  minDegree: MIN_DEG,
+  maxDegree: MAX_DEG,
+};
+
+const dial = JogDial(document.getElementById(VolumeDial.id), config).on('mousemove', e => {
+  const percent = degreeToPercent(e.target.rotation, MIN_DEG, MAX_DEG);
+  handleVolumeChange(percent);
+});
+
+const renderVolumeTicks = numberOfTicks => {
+  const parent = document.getElementById('VolumeTicks');
+  const rotationIncrement = 360 / numberOfTicks;
+  const startRotation = -rotationIncrement;
+  let rotation = startRotation + 180;
+
+  var ticks = '';
+  for (let i = 0; i < numberOfTicks; i++) {
+    let newRotation = rotation + rotationIncrement;
+    ticks += `<div class="tick" style="transform: rotate(${newRotation}deg)"></div>`;
+    rotation = newRotation;
+  }
+  parent.innerHTML = ticks;
+};
+
+renderVolumeTicks(NUM_TICKS);
+dial.angle(START_DEG);
--- server.js
+++ server.js
@@ -1,56 +1,51 @@
-require("dotenv").config();
-const express = require("express");
-const path = require("path");
-const ip = require("ip");
-const PubNub = require("pubnub");
-const qr = require("qrcode-terminal");
-const { audio } = require("system-control");
+const path = require('path');
+const ip = require('ip');
+const qr = require('qrcode-terminal');
+const { audio } = require('system-control');
 
 // Server
+const express = require('express');
 const app = express();
-const port = 9000;
-const ipAddress = ip.address() + ":" + port;
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
-app.use(express.static(path.join(__dirname, "public")));
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-app.listen(port);
+const PORT = 9000;
+const IP = ip.address() + ':' + PORT;
 
-// Generate QR code
-qr.generate("http:/" + ipAddress, function(qrcode) {
-  console.log(qrcode);
-});
-
-// Pubnub
-const uuid = PubNub.generateUUID();
-
-const pubnub = new PubNub({
-  publishKey: process.env.PUBLISH_KEY,
-  subscribeKey: process.env.SUBSCRIBE_KEY,
-  uuid: uuid
-});
-
-// Volume change handler
-const handleReceiveMessage = data => {
-  const { volume, type } = data.message;
-  switch (type) {
-    case "SYSTEM_AUDIO":
-      if (audio.muted()) {
-        audio.muted(false);
-      }
-      audio.volume(volume);
-      console.log("Volume: " + volume + "%");
-    default:
-      break;
+const setVolume = volume => {
+  if (audio.muted()) {
+    audio.muted(false);
   }
+  audio.volume(volume);
 };
 
-pubnub.addListener({
-  message: handleReceiveMessage
-});
-pubnub.subscribe({
-  channels: [process.env.CHANNEL]
+const printVolumePercentage = percentage => {
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  process.stdout.write('Volume: ' + percentage + '%');
+};
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-console.log("Server listening on " + "http://" + ipAddress);
+io.on('connection', socket => {
+  socket.emit('system-volume', audio.volume());
+  socket.on('volume-change', percentage => {
+    if (percentage !== audio.volume()) {
+      setVolume(percentage);
+      printVolumePercentage(percentage);
+      socket.broadcast.emit('system-volume', percentage);
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  qr.generate('http:/' + IP, qrcode => {
+    console.log(qrcode);
+  });
+  console.log('Server listening on ' + 'http://' + IP);
+});
